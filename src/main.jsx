6dae5f03a5c269b6c720b0fdeaee17fbd03393ff
datlt2306 <datlt2306@gmail.com>
2024-09-23 11:13:01 +0700

    WD19201 - Buổi 7

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import App from "./App";
 
-const student = {
-    fullName: "Lê Trọng Đạt",
-    age: 30,
-    address: "Hà Nội",
-    isMarried: false,
-};
-createRoot(document.getElementById("root")).render(
-    <div>
-        <h1>Student Information</h1>
-        <p>Full Name: {student.fullName}</p>
-        <p>Age: {student.age}</p>
-        <p>Address: {student.address}</p>
-        <p>Married: {student.isMarried ? "Chưa lấy vợ" : "Đã chết"}</p>
-    </div>
-);
+createRoot(document.getElementById("root")).render(<App />);
+
+// JSX => Javascript XML

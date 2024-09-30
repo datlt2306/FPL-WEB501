ad0e6251e551b8932d686f5baa41a88f5a1da82d
datlt2306 <datlt2306@gmail.com>
2024-09-30 10:08:32 +0700

    WD19201 - Buổi 10

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,27 @@ import "./App.css";
 
 function App() {
     const [color, setColor] = useState("red");
-    const handleClick = () => {
-        setColor("blue");
-    };
+    const [todos, setTodos] = useState([
+        { id: 1, title: "Learn React" },
+        { id: 2, title: "Learn Vue" },
+        { id: 3, title: "Learn Angular" },
+    ]);
     return (
         <>
-            <button onClick={handleClick}>Change</button>
+            <button onClick={() => setColor("blue")}>Change</button>
             <div style={{ background: color, width: 200, height: 200 }}>Content</div>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>{todo.title}</li>
+                ))}
+            </ul>
+            <button onClick={() => setTodos(todos.filter((todo) => todo.id !== 2))}>
+                Remove Todo
+            </button>
         </>
     );
 }
 export default App;
+
+// Phuương thức .map() => tạo ra mảng mới từ mảng cũ
+// [1,2,3,4].map(function(number){ return number * 2 }) => [2,4,6,8]

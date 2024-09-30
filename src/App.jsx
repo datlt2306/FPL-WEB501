4d03b2bf9e6ff5d57ef15689dbaa977161f01dee
datlt2306 <datlt2306@gmail.com>
2024-09-30 10:44:27 +0700

    WD19201 - Buổi 10

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,51 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-    const [color, setColor] = useState("red");
-    const [todos, setTodos] = useState([
-        { id: 1, title: "Learn React" },
-        { id: 2, title: "Learn Vue" },
-        { id: 3, title: "Learn Angular" },
+    const [products, setProducts] = useState([
+        { id: 1, name: "Iphone 12", price: 1000, status: true }, // item = 0
+        { id: 2, name: "Iphone 13", price: 2000, status: true }, // item = 1
+        { id: 3, name: "Iphone 14", price: 3000, status: false }, // item  = 2
     ]);
+    const removeItem = (id) => {
+        const confirm = window.confirm(`Are you sure you want to delete this product?`);
+        if (confirm) {
+            setProducts(products.filter((item) => item.id !== id));
+        }
+    };
     return (
         <>
-            <button onClick={() => setColor("blue")}>Change</button>
-            <div style={{ background: color, width: 200, height: 200 }}>Content</div>
-            <ul>
-                {todos.map((todo) => (
-                    <li key={todo.id}>
-                        {todo.title}
-                        <button
-                            onClick={() => setTodos(todos.filter((item) => item.id !== todo.id))}
-                        >
-                            Remove Todo
-                        </button>
-                    </li>
-                ))}
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>STT</th>
+                        <th>Tên</th>
+                        <th>Giá</th>
+                        <th>Tình trạng</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((item, index) => (
+                        <tr key={item.id}>
+                            <td>{index + 1}</td>
+                            <td>{item.name}</td>
+                            <td>{item.price}</td>
+                            <td>{item.status ? "Còn hàng" : "Hết hàng"}</td>
+                            <td>
+                                <button onClick={() => removeItem(item.id)}>Xóa</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     );
 }
 export default App;
 
-// Phuương thức .map() => tạo ra mảng mới từ mảng cũ
-// [1,2,3,4].map(function(number){ return number * 2 }) => [2,4,6,8]
+/**
+ * Bước 1: Tạo state là một mảng products
+ * Bước 2: Hiển thị danh sách mảng
+ * Bước 3: Taọ hàm xóa sản phẩm
+ * Bước 4: Click vào button thì lấy id, truyền id vào hàm xóa sản phẩm
+ */

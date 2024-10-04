d7a1c52a5bd0fa36e034b88fda848ac71cfee9b2
datlt2306 <datlt2306@gmail.com>
2024-10-04 10:57:37 +0700

    WD19201 - Buổi 12

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch(`http://localhost:3000/products`)
-            .then((response) => response.json())
-            .then((data) => setProducts(data));
+        (async () => {
+            try {
+                // setProducts(await (await axios.get(`http://localhost:3000/products`)).data);
+                const response = await axios.get(`http://localhost:3000/products`);
+                setProducts(response.data);
+            } catch (error) {
+                console.log(error.message);
+            }
+        })();
     }, []);
+
+    const removeItem = async (id) => {
+        const confirm = window.confirm("Bạn có chắc chắn muốn xóa sản phẩm này không?");
+        if (confirm) {
+            try {
+                await axios.delete(`http://localhost:3000/products/${id}`);
+                setProducts(products.filter((item) => item.id !== id));
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+    };
     return (
         <>
             {products.map((item) => (
-                <div key={item.id}>{item.name}</div>
+                <div key={item.id}>
+                    {item.name} <button onClick={() => removeItem(item.id)}>Xóa</button>
+                </div>
             ))}
         </>
     );

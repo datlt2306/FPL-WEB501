579d54510c5f1e29fa1d7ee56dfb636b99c00683
datlt2306 <datlt2306@gmail.com>
2024-10-28 16:02:32 +0700

    Buổi 1:  Refactor variable and function names for clarity and consistency

---
+++ main.js
@@ -0,0 +1,29 @@
+// Tên biến Danh từ: product, myName, myStatus
+const productName = "Sản phẩm A";
+const productPrice = 200;
+const productInstock = true;
+const productQuantity = 100;
+// Nếu biến là 1 mảng thì tên biến phải là danh từ số nhiều
+const products = [];
+const productList = [];
+const userList = [];
+const roles = [];
+/** Tên hàm là 1 động từ */
+function removeProductById(id) {}
+function addProduct() {}
+function updateProduct() {}
+function getProfileUser(slug) {}
+
+// Demo hàm trả về kết quả
+function sum(a, b) {
+    // Kiểm tra
+    if (typeof a !== "number" || typeof b !== "number") {
+        return "Tham số truyền vào không phải kiểu number";
+    }
+    // Thực thi công việc
+    const result = a + b;
+    // trả về kết quả
+    return result;
+}
+// excute function
+console.log(sum(10, 20));

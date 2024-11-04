07bc53221004596da922cbeda5731aec19a17539
datlt2306 <datlt2306@gmail.com>
2024-11-04 15:39:38 +0700

    WD19305 - Buổi 3: Update main.jsx to display user information instead of the App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const myInfo = {
+    name: "Lê Trọng Đạt",
+    age: 36,
+    isMarried: true,
+};
+createRoot(document.getElementById("root")).render(
+    <div>
+        <h1>Xin chào {myInfo.name}</h1>
+        <p>Tuổi: {myInfo.age}</p>
+        <p>Tình trạng: {myInfo.isMarried ? "đã chết" : "Tuyệt vời"}</p>
+    </div>
+);

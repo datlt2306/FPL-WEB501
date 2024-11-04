ddca7a99457013b30e5e81cd534312dc1745f33c
datlt2306 <datlt2306@gmail.com>
2024-11-04 16:04:06 +0700

    WD19305 - Buổi 3: Refactor main.jsx to use a data object for user information and create a display function for rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,22 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-const myInfo = {
+// dữ liệu
+const data = {
     name: "Lê Trọng Đạt",
     age: 36,
     isMarried: true,
 };
-const myName = "Lê Trọng Đạt";
-
-function sum(a, b) {
-    // kiểm tra
-    if (typeof a !== "number" || typeof b !== "number") {
-        throw new Error("a và b phải là số");
-    }
-    return a + b;
-}
-
-createRoot(document.getElementById("root")).render(
-    <div>
-        <h1>Xin chào {myName}</h1>
-        <p>Tuổi: {myInfo.age}</p>
-        <p>Tình trạng: {myInfo.isMarried ? "đã chết" : "Tuyệt vời"}</p>
-        <p>Tính tổng: {sum(10, 20)}</p>
-    </div>
-);
+// Hàm xử lý
+const displayStudentInfo = (student) => {
+    console.log(student); // { name: "Lê Trọng Đạt", age: 36, isMarried: true}
+    return (
+        <div>
+            <h1>Xin chào: {student.name}</h1>
+            <p>Tuổi: {student.age}</p>
+            <p>Tình trạng: {student.isMarried ? "đã chết" : "Tuyệt vời"}</p>
+        </div>
+    );
+};
+createRoot(document.getElementById("root")).render(<div>{displayStudentInfo(data)}</div>);
+// https://www.youtube.com/@codewithdat

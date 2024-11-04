b27ab8b49093d31fb8f96dc6a75f843723ac6a74
datlt2306 <datlt2306@gmail.com>
2024-11-04 09:35:01 +0700

    WD19305 - Buổi 2: Update student data structure and enhance displayStudentInfo function to handle multiple students

--- ontap/lesson-2/baitap/bai-1.js
+++ ontap/lesson-2/baitap/bai-1.js
@@ -1,18 +1,44 @@
-const student = {
-    name: "Lê Trọng Đạt",
-    age: 36,
-    class: "WD19305",
-    email: "datlt34@fpt.edu.vn",
+const studentList = [
+    {
+        name: "Lê Trọng Đạt",
+        age: 36,
+        class: "WD19305",
+        email: "datlt34@fpt.edu.vn",
+    }, // student
+    {
+        name: "Trần Trung Kiên",
+        age: 26,
+        class: "WD19301",
+        email: "kienph2222@fpt.edu.vn",
+    }, // student
+    {
+        name: "Nguyễn Thúy Quỳnh",
+        age: 33,
+        class: "WD19310",
+        email: "quynhph111@fpt.edu.vn",
+    }, // student
+];
+
+const displayStudentInfo = (students) => {
+    // Kiểm tra
+    if (!Array.isArray(students) || students.length === 0) return "";
+    return students
+        .map((student) => {
+            return ` 
+            <li>Họ và tên: ${student.name}</li>
+            <li>Tuổi: ${student.age}</li>
+            <li>Lớp: ${student.class}</li>
+            <li>Email: ${student.email}</li>
+        `;
+        })
+        .join("");
 };
 
-const displayStudentInfo = (student) => {
-    return `
-        <li>Họ và tên: ${student.name}</li>
-        <li>Tuổi: ${student.age}</li>
-        <li>Lớp: ${student.class}</li>
-        <li>Email: ${student.email}</li>
-    `;
-};
-console.log(displayStudentInfo(student));
+document.querySelector("#content").innerHTML = displayStudentInfo(studentList);
+/**
+ * Bước 1: Tạo object student
+ * Bước 2: Truyền object vào hàm displayStudentInfo
+ * Bước 3: Hiển thị dữ liệu ra ngoài màn hình
+ */
 
-document.querySelector("#content").innerHTML = displayStudentInfo(student);
+// Yêu cầu :  tìm hiểu các phương thức xử lý mảng trong javascript: map, filter, find, reduce
--- ontap/lesson-2/main.js
+++ ontap/lesson-2/main.js
@@ -1,8 +1,9 @@
 // function name - function declaration
+// excute function
+
 function sum(a, b) {
     return a + b;
 }
-// excute function
 console.log("Function name: ", sum(10, 20));
 
 //  function expression
@@ -10,9 +11,15 @@ const sum2 = function (a, b) {
     return a + b;
 };
 console.log("Function Expression:", sum2(10, 20));
-
 //  arrow function
 const sum3 = (a, b) => a + b;
 console.log("Arrow function:", sum3(10, 20));
 
-// Phương pháp học tập: nhìn, hiểu, nhớ, thực hành
+// immediately invoked function expression (IIFE)
+// function tự động gọi ngay khi khai báo
+console.log("IIFE function", ((a, b) => a + b)(10, 20));
+
+// function name
+// function express => khai báo biến có giá trị là 1 function
+// arrow function => cú pháp viết tắt của function expression
+// IIFE => hàm của function expression => khai báo và gọi luôn

4406fb7477ce0530cb114ed9407d27bc1da7c894
datlt2306 <datlt2306@gmail.com>
2024-11-04 15:47:35 +0700

    WD19305 - Buổi 3 : Update main.jsx to display user name and add sum function for calculations

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,26 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 
 const myInfo = {
     name: "Lê Trọng Đạt",
     age: 36,
     isMarried: true,
 };
+const myName = "Lê Trọng Đạt";
+
+function sum(a, b) {
+    // kiểm tra
+    if (typeof a !== "number" || typeof b !== "number") {
+        throw new Error("a và b phải là số");
+    }
+    return a + b;
+}
+
 createRoot(document.getElementById("root")).render(
     <div>
-        <h1>Xin chào {myInfo.name}</h1>
+        <h1>Xin chào {myName}</h1>
         <p>Tuổi: {myInfo.age}</p>
         <p>Tình trạng: {myInfo.isMarried ? "đã chết" : "Tuyệt vời"}</p>
+        <p>Tính tổng: {sum(10, 20)}</p>
     </div>
 );

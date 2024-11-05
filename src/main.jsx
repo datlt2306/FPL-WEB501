5ebabee6674fa0b4170cf45802bee8263772814c
datlt2306 <datlt2306@gmail.com>
2024-11-05 13:48:21 +0700

    WD19302 - Buổi 2: Display student information in main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-);
+const student = {
+    name: "Lê Trọng Đạt",
+    code: "PH09999",
+    age: 36,
+    class: "WD19302",
+};
+function displayStudentInfo(student) {
+    return (
+        <div>
+            <h1>Student Info</h1>
+            <p>Name: {student.name}</p>
+            <p>Code: {student.code}</p>
+            <p>Age: {student.age}</p>
+            <p>Class: {student.class}</p>
+        </div>
+    );
+}
+
+createRoot(document.getElementById("root")).render(displayStudentInfo(student));
+// createRoot(where).render(what)

a557753845bacc5efb8d070312a0ab2a3aab3019
datlt2306 <datlt2306@gmail.com>
2024-11-07 15:32:24 +0700

    WD19303 - Buổi 3: Refactor index.html and main.jsx for improved structure and user information display

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
-import { StrictMode } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import React from "react";
+// B1: Tạo biến user có giá trị là một object chứa thông tin về người dùng
+const user = {
+    firstName: "John",
+    lastName: "Doe",
+};
+// B2: khai báo hàm nhận 1 object và hiển thị thông tin ra ngoài
+function displayName(user) {
+    return user.firstName + " " + user.lastName;
+}
+// B3: khai báo biến element chứa h1 hiển thị thông tin người dùng
+const element = <h1>{displayName(user)}</h1>;
 createRoot(document.getElementById("root")).render(
-    <div className="container">
-        <h1>Hello react</h1>
+    <div>
+        <p>Họ và tên : {element}</p>
     </div>
 );

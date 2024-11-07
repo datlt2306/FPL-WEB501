206d26b9fe62a110759161f888653e1708d71fcc
datlt2306 <datlt2306@gmail.com>
2024-11-07 13:43:17 +0700

    WD19302 - Buổi 4: Refactor App component structure by adding Header, MainContent, and Footer components

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,3 @@
 import ReactDOM from "react-dom/client";
-const user = {
-    first: "John",
-    last: "Doe",
-};
-function displayUser(user) {
-    console.log(user);
-    return user.first + "" + user.last;
-}
-
-const element = <h1>{displayUser(user)}</h1>;
-
-ReactDOM.createRoot(document.querySelector("#root")).render(element);
+import App from "./App";
+ReactDOM.createRoot(document.querySelector("#root")).render(<App />);

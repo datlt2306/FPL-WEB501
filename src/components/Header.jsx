2f4a9506e3b0f7e36c42316713cb074b8a3bd2d8
datlt2306 <datlt2306@gmail.com>
2024-11-11 14:43:07 +0700

    WD19305 - Buổi 6: Add routing with react-router-dom; create HomePage and ProductsPage components; update main entry point to use RouterProvider

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,3 +2,5 @@ function Header() {
     return <h1>Header</h1>;
 }
 export default Header;
+
+// http://localhost:5173/admin/products/add
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,28 @@
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
 
-createRoot(document.getElementById("root")).render(<App />);
+const routers = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomePage />,
+    },
+    {
+        path: "/products",
+        element: <ProductsPage />,
+    },
+]);
+
+createRoot(document.getElementById("root")).render(
+    <>
+        <RouterProvider router={routers} />
+    </>
+);
+
+// B1: npm i react-router-dom
+// B2: Truy cập file main.js
+// chỉnh sửa code như thầy đã viết
+// B3: Cài đặt extension "React+Es7..."
+// B4: Tạo page "rafce"

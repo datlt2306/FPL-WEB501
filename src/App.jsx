c8e2fbd463e951fffc59597bc60aa61e614e6fe6
datlt2306 <datlt2306@gmail.com>
2024-11-11 15:37:24 +0700

    WD19305 - Buổi : - Implement routing with react-router-dom; create Layout components for website and admin; update main entry point to use BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Product from "./components/Product";
+/* eslint-disable react/prop-types */
+import { Link, Outlet, Route, Routes } from "react-router-dom";
+import ProductsPage from "./pages/Products";
+import HomePage from "./pages/Home";
 
+const LayoutWebsite = () => {
+    return (
+        <>
+            <header>Header</header>
+            <main>
+                <Outlet />
+            </main>
+            <footer>Footer</footer>
+        </>
+    );
+};
+const LayoutAdmin = () => {
+    return (
+        <>
+            <aside>Aside</aside>
+            <main>
+                <Outlet />
+            </main>
+        </>
+    );
+};
 const App = () => {
     return (
-        <div>
-            <Header />
-            <main>
-                <Product name="Sản phẩm A" price={200} />
-            </main>
-            <Footer />
-        </div>
+        <>
+            <Link to="/">Home -</Link>
+            <Link to="/admin/products">Admin Products Page - </Link>
+            <Link to="/admin/products/add">Admin Add Products Page</Link>
+            <Routes>
+                <Route path="/" element={<LayoutWebsite />}>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="products" element={<ProductsPage />} />
+                </Route>
+                <Route path="/admin" element={<LayoutAdmin />}>
+                    <Route path="/admin/products" element={<h1>Admin products page</h1>} />
+                    <Route path="/admin/products/add" element={<h1>Admin Add products page</h1>} />
+                </Route>
+            </Routes>
+        </>
     );
 };
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,11 @@
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
-import HomePage from "./pages/Home";
-import ProductsPage from "./pages/Products";
-
-const routers = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomePage />,
-    },
-    {
-        path: "/products",
-        element: <ProductsPage />,
-    },
-]);
-
 createRoot(document.getElementById("root")).render(
-    <>
-        <RouterProvider router={routers} />
-    </>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
 
 // B1: npm i react-router-dom

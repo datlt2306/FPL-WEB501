7ea5009f8135a5c615955c7b7a728def7a916e33
datlt2306 <datlt2306@gmail.com>
2024-11-12 12:31:34 +0700

    WD19302 - Buổi 4 : Add routing with react-router-dom and create HomePage and ProductsPage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
 function App() {
     return (
         <>
-            <Header></Header>
-            <MainContent />
-            <Footer />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/products" element={<ProductsPage />} />
+            </Routes>
         </>
     );
 }
 
 export default App;
+/**
+ * B1: npm i react-router-dom
+ * B2: Truy cập file main.jsx
+ *    import { BrowserRouter } from "react-router-dom";
+ *   ReactDOM.render(<BrowserRouter><App /></BrowserRouter>);
+ * B3: Truy cập file App.jsx
+ *  Định nghĩa ra các đường dẫn cho trang web
+ */
+/**
+ *
+ * Tạo component nhanh bằng câu lệnh : rafce
+ */
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-ReactDOM.createRoot(document.querySelector("#root")).render(<App />);
+import { BrowserRouter } from "react-router-dom";
+ReactDOM.createRoot(document.querySelector("#root")).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);

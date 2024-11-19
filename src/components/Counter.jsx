15fba5e9783ed36b3fcf133be518a97bf2439090
datlt2306 <datlt2306@gmail.com>
2024-11-19 12:07:30 +0700

    WD19305 - Buổi 9: Enhance Counter component to allow removal of friends; add confirmation dialog for deletion

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
 
 const Counter = () => {
-    const [friends] = useState([
+    const [friends, setFriends] = useState([
         { id: 1, name: "Đạt" }, // "bạn" + friend.name
         { id: 2, name: "Kiên" }, // "bạn" +friend.name
         { id: 3, name: "Nhung" }, // "bạn" + friend.name]);
     ]);
+
+    const removeFriend = (id) => {
+        const confirm = window.confirm(`Are you sure????`);
+        if (confirm) {
+            setFriends(friends.filter((friend) => friend.id !== id));
+        }
+    };
     return (
         <div>
             {friends.map((friend) => (
-                <li key={friend.id}>Bạn {friend.name}</li>
+                <li key={friend.id}>
+                    Bạn {friend.name} <button onClick={() => removeFriend(friend.id)}>Xóa</button>
+                </li>
             ))}
         </div>
     );
 };
 
 export default Counter;
+
+// myNumbers = [{id: 1},{ id: 2},{id: 3},{ id: 4}]
+// myNumber.filter((number) => number.id !== 3)
+// newNumbers = [1,2,4]

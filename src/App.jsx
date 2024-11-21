d2b3106cffa79f7c7f1c4afcbdc4d7b8d2272f03
datlt2306 <datlt2306@gmail.com>
2024-11-21 14:31:43 +0700

    WD19303: Add TodoList component and integrate it into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 import LayoutWebsite from "./components/LayoutWebsite";
+import TodoList from "./components/TodoList";
 
 const App = () => {
     return (
         <div>
-            <Routes>
+            <TodoList />
+            {/* <Routes>
                 <Route path="/" element={<LayoutWebsite />}>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/products" element={<ProductsPage />} />
                 </Route>
-            </Routes>
+            </Routes> */}
         </div>
     );
 };
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const TodoList = () => {
+    return (
+        <div className="tw-max-w-xl tw-mx-auto tw-my-10 tw-border-2 tw-border-red-500 tw-p-5">
+            <form className="input-group">
+                <input type="text" className="form-control" placeholder="Learning Javascript" />
+                <button className="btn btn-primary" type="submit">
+                    Thêm mới
+                </button>
+            </form>
+            <h2 className="tw-text-2xl tw-mt-5">Danh sách công việc</h2>
+            <ul className="list-group tw-mt-2">
+                <li className="list-group-item d-flex align-items-center">
+                    <input className="form-check-input me-3" type="checkbox" value="" />
+                    <label className="form-check-label">Học React</label>
+                    <button className="btn btn-sm btn-danger ms-auto">Xóa</button>
+                </li>
+                <li className="list-group-item d-flex align-items-center">
+                    <input type="text" className="form-control input-sm" value="Học Javascript" />
+                    <div className="tw-flex tw-items-center tw-space-x-2 tw-ml-2">
+                        <button className="btn btn-sm btn-info">Hủy</button>
+                        <button className="btn btn-sm btn-primary">Lưu</button>
+                    </div>
+                </li>
+                <li className="list-group-item d-flex align-items-center">
+                    <input className="form-check-input me-3" type="checkbox" value="" />
+                    <label className="form-check-label tw-line-through">Học React</label>
+                    <button className="btn btn-sm btn-danger ms-auto">Xóa</button>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default TodoList;
+
+/**
+ * Bước 1: Tạo giao diện
+ * Bước 2: Fake 1 danh sách và hiển thị ra ngoài
+ * Bước 3: Tạo form và xử lý form để thêm mới todo
+ * Bước 4: Xóa form
+ * Bước 5: Xử lý sự kiện khi click vào checkbox ( completed)
+ */

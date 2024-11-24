351b4137e9be201dba511684de70e8dfd80894b8
datlt2306 <datlt2306@gmail.com>
2024-11-24 14:38:47 +0700

    WD19303 - Enhance admin dashboard and products page with layout and table structure

--- src/components/LayoutAdmin.jsx
+++ src/components/LayoutAdmin.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const LayoutAdmin = () => {
     return (
@@ -54,7 +54,7 @@ const LayoutAdmin = () => {
             </header>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+                    <div className="tw-h-svh sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
                         <div
                             className="offcanvas-md offcanvas-end bg-body-tertiary"
                             tabIndex={-1}
@@ -76,154 +76,27 @@ const LayoutAdmin = () => {
                             <div className="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
                                 <ul className="nav flex-column">
                                     <li className="nav-item">
-                                        <a
+                                        <NavLink
                                             className="nav-link d-flex align-items-center gap-2 active"
                                             aria-current="page"
-                                            href="#"
+                                            to="/admin"
                                         >
                                             <svg className="bi">
                                                 <use xlinkHref="#house-fill" />
                                             </svg>
-                                            Dashboard
-                                        </a>
+                                            Thống kê
+                                        </NavLink>
                                     </li>
                                     <li className="nav-item">
-                                        <a
+                                        <NavLink
                                             className="nav-link d-flex align-items-center gap-2"
-                                            href="#"
+                                            to="/admin/products"
                                         >
                                             <svg className="bi">
                                                 <use xlinkHref="#file-earmark" />
                                             </svg>
-                                            Orders
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link d-flex align-items-center gap-2"
-                                            href="#"
-                                        >
-                                            <svg className="bi">
-                                                <use xlinkHref="#cart" />
-                                            </svg>
-                                            Products
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link d-flex align-items-center gap-2"
-                                            href="#"
-                                        >
-                                            <svg className="bi">
-                                                <use xlinkHref="#people" />
-                                            </svg>
-                                            Customers
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link d-flex align-items-center gap-2"
-                                            href="#"
-                                        >
-                                            <svg className="bi">
-                                                <use xlinkHref="#graph-up" />
-                                            </svg>
-                                            Reports
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link d-flex align-items-center gap-2"
-                                            href="#"
-                                        >
-                                            <svg className="bi">
-                                                <use xlinkHref="#puzzle" />
-                                            </svg>
-                                            Integrations
-                                        </a>
-                                    </li>
-                                </ul>
-                                <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
-                                    <span>Saved reports</span>
-                                    <a
-                                        className="link-secondary"
-                                        href="#"
-                                        aria-label="Add a new report"
-                                    >
-                                        <svg className="bi">
-                                            <use xlinkHref="#plus-circle" />
-                                        </svg>
-                                    </a>
-                                </h6>
-                                <ul className="nav flex-column mb-auto">
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link d-flex align-items-center gap-2"
-                                            href="#"
-                                        >
-                                            <svg className="bi">
-                                                <use xlinkHref="#file-earmark-text" />
-                                            </svg>
-                                            Current month
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link d-flex align-items-center gap-2"
-                                            href="#"
-                                        >
-                                            <svg className="bi">
-                                                <use xlinkHref="#file-earmark-text" />
-                                            </svg>
-                                            Last quarter
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link d-flex align-items-center gap-2"
-                                            href="#"
-                                        >
-                                            <svg className="bi">
-                                                <use xlinkHref="#file-earmark-text" />
-                                            </svg>
-                                            Social engagement
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link d-flex align-items-center gap-2"
-                                            href="#"
-                                        >
-                                            <svg className="bi">
-                                                <use xlinkHref="#file-earmark-text" />
-                                            </svg>
-                                            Year-end sale
-                                        </a>
-                                    </li>
-                                </ul>
-                                <hr className="my-3" />
-                                <ul className="nav flex-column mb-auto">
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link d-flex align-items-center gap-2"
-                                            href="#"
-                                        >
-                                            <svg className="bi">
-                                                <use xlinkHref="#gear-wide-connected" />
-                                            </svg>
-                                            Settings
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link d-flex align-items-center gap-2"
-                                            href="#"
-                                        >
-                                            <svg className="bi">
-                                                <use xlinkHref="#door-closed" />
-                                            </svg>
-                                            Sign out
-                                        </a>
+                                            Sản phẩm
+                                        </NavLink>
                                     </li>
                                 </ul>
                             </div>
@@ -231,34 +104,6 @@ const LayoutAdmin = () => {
                     </div>
                     <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                         <Outlet />
-                        {/* <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                            <h1 className="h2">Dashboard</h1>
-                            <div className="btn-toolbar mb-2 mb-md-0">
-                                <div className="btn-group me-2">
-                                    <button
-                                        type="button"
-                                        className="btn btn-sm btn-outline-secondary"
-                                    >
-                                        Share
-                                    </button>
-                                    <button
-                                        type="button"
-                                        className="btn btn-sm btn-outline-secondary"
-                                    >
-                                        Export
-                                    </button>
-                                </div>
-                                <button
-                                    type="button"
-                                    className="btn btn-sm btn-outline-secondary dropdown-toggle d-flex align-items-center gap-1"
-                                >
-                                    <svg className="bi">
-                                        <use xlinkHref="#calendar3" />
-                                    </svg>
-                                    This week
-                                </button>
-                            </div>
-                        </div> */}
                     </main>
                 </div>
             </div>
--- src/pages/admin/AdminProductsPage.jsx
+++ src/pages/admin/AdminProductsPage.jsx
@@ -1,7 +1,55 @@
 import React from "react";
 
 const AdminProductsPage = () => {
-    return <div>AdminProductsPage</div>;
+    return (
+        <div>
+            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 className="h2">Quản lý sản phẩm</h1>
+                <div className="btn-toolbar mb-2 mb-md-0">
+                    <div className="btn-group me-2">
+                        <button type="button" className="btn btn-sm btn-outline-primary">
+                            Thêm sản phẩm
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>STT</th>
+                        <th>Ảnh</th>
+                        <th>Tên</th>
+                        <th>Giá</th>
+                        <th>Trạng thái</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td width={50}>1</td>
+                        <td width={60}>
+                            <img
+                                src="https://picsum.photos/id/1/300/300"
+                                alt="sản phẩm 1"
+                                width={50}
+                                height={50}
+                            />
+                        </td>
+                        <td>Sản phẩm 1</td>
+                        <td>$100</td>
+                        <td>
+                            <span className="badge text-bg-success">Còn hàng</span>
+                            <span className="badge text-bg-secondary">Hết hàng</span>
+                        </td>
+                        <td width={250}>
+                            <button className="btn btn-outline-danger">Xóa</button>
+                            <button className="btn btn-outline-primary ms-1">Cập nhật</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 export default AdminProductsPage;

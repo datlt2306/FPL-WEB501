f1f90c0123af68e64512209c20678a61b085465a
datlt2306 <datlt2306@gmail.com>
2024-11-25 15:02:07 +0700

    WD19305 - Add product table to AdminProductsPage; remove commented-out code from LayoutAdmin

--- src/pages/admin/AdminProductsPage.jsx
+++ src/pages/admin/AdminProductsPage.jsx
@@ -16,6 +16,60 @@ const AdminProductsPage = () => {
                     </div>
                 </div>
             </div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Ảnh</th>
+                        <th>Tên</th>
+                        <th>Giá</th>
+                        <th>Tình trạng</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td width={60}>
+                            <img
+                                src="https://picsum.photos/id/1/300/300"
+                                alt="Sản phẩm 1"
+                                width={50}
+                                height={50}
+                            />
+                        </td>
+                        <td>Sản phẩm 1</td>
+                        <td>$100</td>
+                        <td>
+                            <span className="badge text-bg-success">Còn hàng</span>
+                        </td>
+                        <td width={250}>
+                            <button className="btn btn-outline-danger">Xóa</button>
+                            <button className="btn btn-outline-primary ms-2">Cập nhật</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td width={60}>
+                            <img
+                                src="https://picsum.photos/id/2/300/300"
+                                alt="Sản phẩm 1"
+                                width={50}
+                                height={50}
+                            />
+                        </td>
+                        <td>Sản phẩm 2</td>
+                        <td>$200</td>
+                        <td>
+                            <span className="badge text-bg-secondary">Hết hàng</span>
+                        </td>
+                        <td width={250}>
+                            <button className="btn btn-outline-danger">Xóa</button>
+                            <button className="btn btn-outline-primary ms-2">Cập nhật</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };

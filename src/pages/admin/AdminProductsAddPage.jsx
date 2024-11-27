f1d5b9b1fa4ef6fc94ae81176002fbcdfeb03977
datlt2306 <datlt2306@gmail.com>
2024-11-27 14:46:21 +0700

    WD19305 - Add product form functionality to AdminProductsAddPage; integrate react-hook-form for validation and submission

--- src/pages/admin/AdminProductsAddPage.jsx
+++ src/pages/admin/AdminProductsAddPage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const AdminProductsAddPage = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onHandleSubmit = (formData) => {
+        fetch(`http://localhost:3000/products`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        }).then(() => {
+            alert("Thêm sản phẩm thành công");
+        });
+    };
     return (
         <div>
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -15,15 +33,94 @@ const AdminProductsAddPage = () => {
                         </Link>
                     </div>
                 </div>
-                {/* 
-                    Tên - input type text
-                    Giá - input type number
-                    Ảnh - input type text
-                    Tình trạng - input type radio
-                    Mô tả - textarea
-                    button - type submit
-                */}
             </div>
+            <form className="w-50 m-auto" onSubmit={handleSubmit(onHandleSubmit)}>
+                <div className="mb-3">
+                    <label htmlFor="" className="form-label">
+                        Tên sản phẩm
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Sản phẩm A"
+                        {...register("name", { required: true, minLength: 5 })}
+                    />
+                    {errors?.name?.type === "required" && (
+                        <span className="text-danger">Bắt buộc nhập</span>
+                    )}
+                    {errors?.name?.type === "minLength" && (
+                        <span className="text-danger">Ít nhất 5 ký tự</span>
+                    )}
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="" className="form-label">
+                        Giá sản phẩm
+                    </label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        placeholder="$200"
+                        {...register("price", { required: true, min: 0 })}
+                    />
+                    {errors?.price?.type === "required" && (
+                        <span className="text-danger">Bắt buộc nhập</span>
+                    )}
+                    {errors?.price?.type === "min" && (
+                        <span className="text-danger">Giá không được để âm</span>
+                    )}
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="" className="form-label">
+                        Tình trạng
+                    </label>
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            value={true}
+                            id="flexRadioDefault1"
+                            defaultChecked
+                            {...register("inStock")}
+                        />
+                        <label className="form-check-label" htmlFor="flexRadioDefault1">
+                            Còn hàng
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            value={false}
+                            {...register("inStock")}
+                            id="flexRadioDefault2"
+                        />
+                        <label className="form-check-label" htmlFor="flexRadioDefault2">
+                            Hết hàng
+                        </label>
+                    </div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="" className="form-label">
+                        Ảnh sản phẩm
+                    </label>
+                    <input type="text" className="form-control" {...register("imageUrl")} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="" className="form-label">
+                        Mô tả sản phẩm
+                    </label>
+                    <textarea
+                        className="form-control"
+                        rows={4}
+                        {...register("description")}
+                    ></textarea>
+                </div>
+                <div className="mb-3">
+                    <button className="btn btn-primary" type="submit">
+                        Thêm mới
+                    </button>
+                </div>
+            </form>
         </div>
     );
 };

b820e231002a7bb95e9b4052637800671bc409df
datlt2306 <datlt2306@gmail.com>
2025-01-08 15:13:39 +0700

    add: implement functions for finding max, reversing strings, and filtering even numbers

--- ontap/lession-2/main.js
+++ ontap/lession-2/main.js
@@ -18,3 +18,36 @@ const myName = {
 function showProducts() {}
 function removeProduct(id) {}
 function getProductById(id) {}
+// Bài tập
+const friendList = ["Đạt", "Minh", "Hòa", "Ngọc"];
+const profile = {
+    name: "Đạt",
+    age: 30,
+    email: "datlt34@fpt.edu.vn",
+};
+
+const resultTotalFromZeroToHundred = 5050;
+
+// 2
+// [1,5,6,12,7,9,10,15,20,30,50,100]
+function findMax(numbers) {
+    if (!Array.isArray(numbers)) return "Dữ liệu không đúng";
+    return Math.max(...numbers);
+}
+function reverseString(name) {
+    // Đạt => ['Đ', 'ạ', 't] => ['t', 'ạ', 'Đ'] => 'tạĐ'
+    if (typeof name !== "string") return "Dữ liệu không đúng";
+    return name.split("").reverse().join("");
+}
+function filterEvenNumbers(numbers) {
+    if (!Array.isArray(numbers)) return "Dữ liệu không đúng";
+    return numbers.filter(function (number) {
+        return number % 2 === 0;
+    });
+}
+
+console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+// dùng chatgpt, đọc hiểu từng dòng
+// ( validate, processing, return)
+// ngắn và tốt nhất có thể

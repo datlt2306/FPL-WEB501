7141ddafaebd2fccfcb1ab7406fcf367a85a4253
datlt2306 <datlt2306@gmail.com>
2025-01-08 14:34:22 +0700

    add: create initial HTML and JavaScript files for lesson 2

---
+++ ontap/lession-2/main.js
@@ -0,0 +1,20 @@
+// Đặt tên biến
+// Danh từ
+
+const roles = ["Admin", "Editor", "User"];
+const student = {
+    name: "John",
+    age: 20,
+    role: "Admin",
+};
+const studentList = [];
+const isStatus = true;
+const myName = {
+    age: 20,
+};
+
+// Động từ
+
+function showProducts() {}
+function removeProduct(id) {}
+function getProductById(id) {}

f8c23551126659c2bfcfdb212626d1bd1a91702e
datlt2306 <datlt2306@gmail.com>
2025-01-13 15:34:54 +0700

    add: integrate Bootstrap for styling and update user info display in main.jsx

--- reactjs/src/main.jsx
+++ reactjs/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 const a = 10;
 const b = 20;
 const myInfo = {
@@ -21,10 +22,20 @@ function showInfo(user) {
         </div>
     );
 }
-
+// jsx => javascript xml
 // document.querySelector('#root').innerHTML = a + b;
 createRoot(document.getElementById("root")).render(
     <div>
-        <p>Hàm info: {showInfo(myInfo)}</p>
+        <div className="card" style={{ width: "18rem" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">{myInfo.name}</h5>
+                <p className="card-text">{myInfo.age}</p>
+                <a href="#" className="btn btn-primary">
+                    Go somewhere
+                </a>
+            </div>
+        </div>
     </div>
 );
+// <div>ul + h1</div>

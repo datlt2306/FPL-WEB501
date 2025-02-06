4a5b5daa4178246773d02e395538c053790f6a49
datlt2306 <datlt2306@gmail.com>
2025-02-06 17:33:08 +0700

    add: implement fetchData function and update buyRing function for improved error handling

--- ontap/lession-7/asynconus.js
+++ ontap/lession-7/asynconus.js
@@ -1,3 +1,29 @@
+// function loadScript(src, callback) {
+//     let script = document.createElement("script");
+//     script.src = src;
+//     script.onload = () => callback(script);
+//     document.head.append(script);
+// }
+// loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js", (script) => {
+//     console.info(`Cool, the script ${script.src} is loaded`);
+// }); // setTimeout();
+
+function fetchData(callback) {
+    console.log("Đang lấy dữ liệu...");
+
+    setTimeout(() => {
+        const data = { id: 1, name: "Alice", age: 25 };
+        console.log("Dữ liệu đã tải xong!");
+        callback(data);
+    }, 2000);
+}
+// Callback để xử lý dữ liệu
+function displayData(data) {
+    console.log("Dữ liệu nhận được:", data);
+}
+
+// Ví dụ 2
+
 function buyRing(callback, errorCallback) {
     console.log("🛍️ Đang đi mua nhẫn... (chờ 2 giây)");
     setTimeout(() => {
@@ -9,7 +35,7 @@ function buyRing(callback, errorCallback) {
 function bookRestaurant(callback, errorCallback) {
     console.log("🍽️ Đang đặt bàn nhà hàng... (chờ 2 giây)");
     setTimeout(() => {
-        let success = Math.random() > 0.3;
+        let success = Math.random(`r`) > 0.3;
         success ? callback() : errorCallback("🚫 Nhà hàng hết chỗ...");
     }, 2000);
 }

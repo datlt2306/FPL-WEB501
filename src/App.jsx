0eabaafc4f2b169afbe86878d54c8e17acdad30f
datlt2306 <datlt2306@gmail.com>
2025-11-26 15:14:17 +0700

    feat(router): integrate react-router-dom for navigation\n\n- Added react-router-dom to manage routing within the application.\n- Updated App component to include navigation links and routes for home and about pages.\n- Wrapped App component with BrowserRouter in main.jsx to enable routing functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import DemoUseEffect from "./components/DemoUseEffect";
 
 function App() {
-    return <DemoUseEffect />;
+    return (
+        <div>
+            <Link to="/">Trang chủ</Link>
+            <Link to="/about">Trang giới thiệu</Link>
+            <Routes>
+                <Route path="/" element={<h1>Trang chủ</h1>} />
+                <Route path="about" element={<h1>Trang giới thiệu</h1>} />
+            </Routes>
+        </div>
+    );
 }
+// npm i  react-router-dom
+// gắn component <BrowserRouter /> ở main.jsx
+// code
 
 export default App;

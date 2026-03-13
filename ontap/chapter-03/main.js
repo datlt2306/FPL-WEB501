a175ab6d1fb4ad1137147e2cd6901c9cb3435f92
datlt2306 <datlt2306@gmail.com>
2026-03-13 13:01:55 +0700

    Refactor index.html to change language to English, update title, and simplify structure by removing unnecessary elements while adding a new script for functionality.

---
+++ ontap/chapter-03/main.js
@@ -0,0 +1,27 @@
+// DOM - querySelector và querySelectorAll, getElementById
+
+const data = ['Việc 1', 'Việc 2', 'Việc 3', 'Việc 4'];
+
+const todos = document.querySelector('#todos');
+
+let content = "";
+// for loop
+// for (let i = 0; i < data.length; i++) {
+//     content += `<li>${data[i]}</li>`;
+//     console.log("content", content);
+// }
+
+// for...in
+// for (let i in data) {
+//     content += `<li>${data[i]}</li>`;
+// }
+
+// for...of
+// for (let item of data) {
+//     content += `<li>${item}</li>`;
+// }
+
+data.forEach((item, index) => {
+    content += `<li>${item} - ${index}</li>`;
+})
+todos.innerHTML = content;
